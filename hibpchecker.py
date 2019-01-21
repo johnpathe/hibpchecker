771759085a25e3f59cbc38f520b067b5a50b6207
johnpathe <johnpathe@gmail.com>
2019-01-21 16:07:09 +1100

    Add files via upload
    Tweaked output a little. Less text clutter on 0 results.
    Now includes what types of data was involved in each breach.

--- hibpchecker.py
+++ hibpchecker.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # 
-# TODO LIST:
 #allow input from file at command line
 #incorporate other dump checkers sites - ghostproject.fr, OTHER?
 #incorporate cred stuffer program
@@ -8,14 +7,26 @@
 	#verify email addr supplied at command line is valid
 #allow user to supply a filename
 	#specify file format (one email addr per line)
+
 #make this work with just regular python requests instead of using this HIBP library imported below
-
-
+import json
 
 # MUST INSTALL THIS FIRST!!!
 #using this library https://github.com/kernelmachine/haveibeenpwned
 
 from hibp import HIBP
+
+
+'''
+hibp execute() returns a list. each item in list is each breach this account appears in
+'''
+
+'''
+Inside each list item is a dict() with the following keys:
+[u'PwnCount', u'Domain', u'IsSensitive', u'Name', u'Title', u'DataClasses', u'IsRetired', u'IsSpamList', u'BreachDate', u'IsFabricated', 
+#u'ModifiedDate', u'LogoPath', u'AddedDate', u'IsVerified', u'Description']
+
+'''
 
 def pwnedChecker(account):
 	req1 = HIBP.get_account_breaches(account)
@@ -24,13 +35,15 @@ def pwnedChecker(account):
 	print 'Checking Account: %s ...' % account
 
 	if isinstance(result,str):
-		print 'It appears %s was not involved in any breaches.' % account
-		print 'Response from HIBP API: %s' % result
+		print 'Email address not involved in any breaches on haveibeenpwned.com'
 	if isinstance(result,list):
 		print 'Number of breaches this account appears in %d' % len(result)
 		for breach in result:
 			print 'Breach Name: %s' % breach['Name']
 			print 'Breach Date: %s' % breach['BreachDate']
+			print 'Data Classes:'
+			for classType in breach['DataClasses']:
+				print '  %s' % classType
 	print '\n'
 
 #form a list of accounts to check on HIBP
